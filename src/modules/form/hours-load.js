7ad44c91276e9b042e56497005283e6de2c76c13
igor gomes <gomesoliveirasl02@hotmail.com>
2025-03-05 20:34:53 -0300

    add hour in interface

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,17 +1,28 @@
-import  dayjs  from "dayjs"
-import { openingHours } from "../../utils/opening-hours"
+import dayjs from "dayjs";
+import { openingHours } from "../../utils/opening-hours";
 
-export function hoursLoad({ date }){
-  const opening = openingHours.map((hour)=>{
+const hours = document.getElementById("hours")
+
+export function hoursLoad({ date }) {
+  const opening = openingHours.map((hour) => {
     // Recupera somente a hora
-    const [scheduleHour, _] = hour.split(":")
+    const [scheduleHour, _] = hour.split(":");
 
     // adiciona a hora na date e verifica se esta no futuro
-    const isHoursPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+    const isHoursPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
 
-    return{
+    return {
       hour,
       available: isHoursPast,
-    }
-  })
+    };
+  });
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+    li.textContent = hour
+    hours.append(li)
+  });
 }
